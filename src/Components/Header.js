611931869aa3177f2c0687f80e70ad08eaf368e1
vgangarapu <vgangarapu@am.sony.com>
2020-05-27 08:45:34 +0530

    Blog and Apple changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,6 +28,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="" href="https://vaishnavscript.wordpress.com/">Blog</a></li>
          </ul>
 
       </nav>
